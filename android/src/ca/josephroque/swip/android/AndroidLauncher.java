33cd210745f17c22996f788a862c63de2678d0be
Joseph Roque <github@josephroque.ca>
2015-08-22 09:29:38 -0400

    Initial libgdx project commit

---
+++ android/src/ca/josephroque/swip/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ca.josephroque.swip.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ca.josephroque.swip.SwipGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SwipGame(), config);
+	}
+}
